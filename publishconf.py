1beb6249dc9b0662e9782c4a351f791e26ba9ca6
Thomas Graf <git@thomasgraf.net>
2016-07-29 01:49:21 -0400

    fix MENUITEMS to also include Home

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'http://thomasgraf.net'
-MENUITEMS = [('News', SITEURL + '/index.html')]
+MENUITEMS = [('Home', SITEURL),
+             ('News', SITEURL + '/news.html')]
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 
