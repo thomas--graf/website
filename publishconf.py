361a4ec44fe05961d02436c7edfcceb179c04598
Thomas Graf <git@thomasgraf.net>
2019-03-06 00:36:31 -0500

    switch to https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://thomasgraf.net'
+SITEURL = 'https://thomasgraf.net'
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 DELETE_OUTPUT_DIRECTORY = False
 
